1495816532f3c2a3e361d5a491bc555c2456a45d
jandono <andonov92@gmail.com>
2024-12-01 18:21:50 +0100

    2024 Day 1

---
+++ 2024/1/1.2.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+def read_int(): return int(input().strip())
+def read_ints(): return list(map(int, input().strip().split()))
+def read_string(): return input()
+def read_strings(): return input().strip().split()
+def read_file(path: str):
+  with open(path, 'r') as f:
+    content = f.read()
+    return content
+
+def solve():
+  l1, l2 = [], []
+  while True:
+    try:
+      nums = read_ints()
+      l1.append(nums[0])
+      l2.append(nums[1])
+    except Exception:
+      break
+
+  c = Counter(l2)
+  s = 0
+  for i in range(len(l1)):
+    s += l1[i] * c[l1[i]]
+  print(s)
+
+if __name__ == '__main__':
+  solve()
